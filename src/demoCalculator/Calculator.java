d7a34a2bf7f20993eddb99bd51909c44f05aa30d
vokhoa1998 <58552724+vokhoa1998@users.noreply.github.com>
2020-06-05 16:23:17 +0700

    up-chuc-nang-tinh-toan

--- src/demoCalculator/Calculator.java
+++ src/demoCalculator/Calculator.java
@@ -1,10 +1,17 @@
 package demoCalculator;
 
 public class Calculator {
-
+    void handleAdd(int a, int b) {
+    	return a + b;
+    }
+    void handleMinus(int a, int b) {
+    	return a - b;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     Calculator c = new Calculator()
+    c.handleAdd(10, 9);
+     c.handleMinus(10, 9);
 	}
 
 }
